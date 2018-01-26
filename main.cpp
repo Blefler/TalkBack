7cc5b2ef1860f06290a29f03156614aeda2fb0e5
Kevin Buffardi <kbuffardi@ecc-jaguar.csuchico.edu>
2018-01-26 14:20:02 -0800

    Initial program compiles

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main()
+{
+  std::cout<<"Welcome!";
+  std::cout<<"This is the first\nC++ command-line application";
+  std::cout<<"for CSCI-111 with Dr. Buffardi."<<std::endl;
+  std::cout<<"Thank you for your time. Goodbye."<<std::endl;
+
+  return 0;
+}
